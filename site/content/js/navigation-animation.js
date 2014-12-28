f65323331f9a7181039bb2ca145f915385d50c9f
Thom Castermans <thomcastermans@users.sourceforge.net>
2014-12-28 18:34:28 +0100

    Animations for navigation.
    When clicking an animation, the page content is changed in an animated fashion
    now. This only happens when the browser supports the History API and JS is
    enabled, otherwise no animations are applied.

---
+++ site/content/js/navigation-animation.js
@@ -0,0 +1,87 @@
+---
+yuiCompress: !!bool false
+---
+(function($, window, document) {
+
+        /**
+         * jQuery event handler that should be triggered when navigating has
+         * completed ('denvelop-navigated' event). This will animate some icons.
+         */
+    var animate = function(event, prevPage, newPage) {
+            // TODO
+            gearRotating = false;
+        },
+        /**
+         * jQuery event handler that should be triggered when navigating starts
+         * ('denvelop-navigating' event). This will show a spinning cog-wheel.
+         */
+        animateLoading = function(event) {
+            gearRotating = true;
+            rotateSomewhat();
+            gearObj.stop().animate({'top': 0}, 150);
+        },
+        gear = null,
+        gearObj = null,
+        gearRotating = false,
+        rotateSomewhat = function() {
+            gear.stop().animate({'transform': 'r45,80,96'}, 1000, function() {
+                if (gearRotating) {
+                    // reset transformation and repeat it (so it looks infinite)
+                    gear.attr({'transform': 'rotate(0 80 96)'});
+                    rotateSomewhat();
+                } else {
+                    // stop, but keep it spinning while it goes down
+                    gear.stop().animate({'transform': 'r45,80,96'}, 1000);
+                    gearObj.stop().animate({'top': 5}, 150);
+                }
+            });
+        },
+        /**
+         * Function that splits the envelope SVG in a foreground and background,
+         * so that icons can be put 'in' the envelope.
+         */
+        splitEnvelope = function() {
+            var bgWrapper = $('#envelope'),
+                bg = Snap(bgWrapper.get(0)),
+                fgWrapper = bgWrapper.clone();
+            fgWrapper.addClass('logo-layer-fg')
+                .on('load', function() {
+                    var fg = Snap(fgWrapper.get(0));
+                    fg.selectAll('path:not(:last-child)').remove();
+                    bg.selectAll('path:last-child').remove();
+                })
+                .appendTo($('#logo'));
+            // append a margin element to cover icons sticking out of the bottom
+            // of the envelope
+            var marginEl = $('<div />');
+            marginEl.css({
+                'position': 'absolute',
+                'top': parseInt(bgWrapper.css('marginTop'))
+                        + bgWrapper.height(),
+                'right': 0,
+                'bottom': 0,
+                'left': 0,
+                'z-index': 3,
+                'background': $('#header').css('background-color')
+            });
+            $('#logo').append(marginEl);
+        };
+
+    $(function() {
+        // make gear icon globally available for easy access
+        gearObj = $('#loading-gear');
+        gear = Snap(gearObj.get(0)).select('g');
+        gearObj.css({'display': 'block', 'top': 5});
+        // split the envelope icon in parts, so that we can move icons in and
+        // out of the envelope easily and also add a margin element that covers
+        // the gear when it moves out of the envelope (and other icons too)
+        splitEnvelope();
+        // bind to the custom event that is fired as soon as a navigation link
+        // is clicked and intercepted, but the page must still be loaded
+        $(window).on('denvelop-navigating', animateLoading);
+        // bind to the custom event that is fired when navigation is changed
+        // and the event could be intercepted and handled using JS
+        $(window).on('denvelop-navigated', animate);
+    });
+
+}(jQuery, window, document));
--- site/content/js/navigation.js
+++ site/content/js/navigation.js
@@ -1,15 +1,21 @@
 ---
-yuiCompress: !!bool false
+yuiCompress: !!bool true
 ---
 (function($, window, document) {
 
-    /**
-     * This function should be called in the jQuery click handler of a link
-     * element (`a`). It will try to use AJAX and history magic to keep page
-     * loading asynchronous. If it succeeds, it wil fire some fancy animations.
-     */
-    var navigate = function() {
+        /**
+         * Currently loaded page information.
+         */
+    var curPage,
+        /**
+        * This function should be called in the jQuery click handler of a link
+        * element (`a`). It will try to use AJAX and history magic to keep page
+        * loading asynchronous. If it succeeds, it wil fire an event that may
+        * trigger some fancy animations, who knows.
+        */
+        navigate = function() {
             if (historySupport) {
+                $(window).trigger('denvelop-navigating');
                 $.ajax({
                     url: $(this).attr('href') + 'page.json',
                     success: updatePage,
@@ -51,27 +57,75 @@ yuiCompress: !!bool false
                 addHistoryState = true;
             }
 
+            // update document content and header, re-attach handlers in header
             document.title = page.title;
-            $('#content').html(page.html);
+            updatePageContent(page);
             updateHeaderLinks(false);
             $('#header').children(':not(#logo)').remove();
             $('#header').append($(page.header).filter(':not(#logo)'));
             updateHeaderLinks();
 
+            // possibly push the new state on the history stack
             if (addHistoryState) {
                 window.history.pushState(page, page.title, page.url);
             }
-        };
+
+            // swap curPage and trigger custom event
+            $(window).trigger('denvelop-navigated', [curPage, page]);
+            curPage = page;
+        },
+        /**
+         * Update the page content using an animation.
+         */
+        updatePageContent = function(page) {
+            var content = $('#content'),
+                contentM = parseInt(content.css('margin-left')),
+                contentP = parseInt(content.css('padding-left')),
+                contentW = content.width(),
+                newContent = $('<div />', { html: page.html }),
+                contentWrap = $('<div />'),
+                contentWrapWrap = $('<div />');
+            contentWrap.css({
+                width: contentW * 2 + contentM * 4
+            });
+            contentWrapWrap.css({
+                width: contentW + contentM * 2,
+                overflow: 'hidden',
+                padding: contentP
+            });
+            content.add(newContent).css({
+                display: 'inline-block',
+                verticalAlign: 'top',
+                width: contentW,
+                margin: contentM,
+                padding: contentP
+            });
+            content.wrap(contentWrapWrap);
+            content.wrap(contentWrap);
+            content.after(newContent);
+            content.parent().css('marginLeft', 0).animate({
+                marginLeft: -contentW - contentM * 2
+            }, function() {
+                // actually remove the old content now that it is off screen
+                content.parent().css('marginLeft', 0)
+                content.remove();
+                // make the newly shown content the actual #content
+                newContent.unwrap().unwrap()
+                    .attr('id', 'content')
+                    .removeAttr('style');
+            });
+        }
 
     $(function() {
         if (historySupport) {
             // save the current state
-            window.history.replaceState({
+            curPage = {
                 title: document.title,
-                url: '',
+                url: window.location.pathname,
                 header: $('#header').html(),
                 html: $('#content').html()
-            }, document.title, '');
+            };
+            window.history.replaceState(curPage, document.title, '');
             // process all navigational links to use AJAX
             updateHeaderLinks();
             // attach an event handler for when the user uses 'prev' and 'next'
