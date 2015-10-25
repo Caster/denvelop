164fa9595ac9e26ddcdcb2ee699642cfada7cbb9
Thom Castermans <thomcastermans@users.sourceforge.net>
2015-10-25 12:16:39 +0100

    Add a lot of Dutch content.

--- site/lib/helpers/logo.rb
+++ site/lib/helpers/logo.rb
@@ -1,7 +1,9 @@
 module LogoHelper
 
-    def logo(name)
-        '<img class="logo" src="' + @items['/img/logos/' + name + '.*'].path + '" alt="' + name + ' logo" title="' + name + ' logo" />'
+    def logo(name, href = nil)
+        (href.nil? ? '' : '<a href="' + href + '">') +
+        '<img class="logo" src="' + @items['/img/logos/' + name.downcase + '.*'].path + '" alt="' + name + ' logo" title="' + name + ' logo" />' +
+        (href.nil? ? '' : '</a>')
     end
 
 end
--- site/lib/helpers/nav.rb
+++ site/lib/helpers/nav.rb
@@ -4,4 +4,9 @@ module NavHelper
         'data-id="' + item[:id] + '" data-url="' + item.path + '"'
     end
 
+    def nav_link(item, text)
+        '<a class="nav-link" ' + nav_attrs(item) + ' href="' +
+                item.path + '">' + text + '</a>'
+    end
+
 end
