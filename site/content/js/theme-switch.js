4739faa59c4fca999fac27e2cd5bed95730c7d2a
Thom Castermans <thomcastermans@users.sourceforge.net>
2015-11-04 20:47:52 +0100

    Save chosen theme in local storage.

--- site/content/js/theme-switch.js
+++ site/content/js/theme-switch.js
@@ -6,6 +6,79 @@ yuiCompress: !!bool true
     var $head = $('head'),
         curTheme = null,
         curThemeIsInverse = false,
+        haveLocalStorage = null,
+
+    /**
+     * \url https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+     */
+    storageAvailable = function(type) {
+        try {
+            var storage = window[type],
+                x = '__storage_test__';
+            storage.setItem(x, x);
+            storage.removeItem(x);
+            return true;
+        } catch(e) {
+            return false;
+        }
+    },
+
+    switchTo = function(theme, isInverse) {
+        // save theme name globally
+        curTheme = theme;
+        curThemeIsInverse = isInverse;
+        themeStorage('save');
+        // do actual switch
+        updateStylesheets();
+        updateImages();
+    },
+
+    /**
+     * Function that can be used to access the local storage for loading/saving
+     * the theme and if it is an inverted theme.
+     *
+     * \param action String indicating what to do. Can be 'load' or 'save'.
+     * \return A boolean indicating if the action was successful.
+     */
+    themeStorage = function(action) {
+        // check support if not done yet
+        if (haveLocalStorage === null) {
+            haveLocalStorage = storageAvailable('localStorage');
+        }
+        // if no support, do nothing
+        if (!haveLocalStorage) {
+            return false;
+        }
+        // either load or save the theme from/to storage
+        if (action === 'load') {
+            var storage = window.localStorage,
+                theme = storage.getItem('themeName'),
+                themeInverted = storage.getItem('themeInverted');
+            if (theme !== null && themeInverted !== null) {
+                curTheme = theme;
+                curThemeIsInverse = (themeInverted === '1');
+                return true;
+            }
+            return false;
+        } else if (action === 'save') {
+            var storage = window.localStorage;
+            storage.setItem('themeName', curTheme);
+            storage.setItem('themeInverted', (curThemeIsInverse ? '1' : '0'));
+            return true;
+        }
+        // unknown action
+        return false;
+    },
+
+    /**
+     * Update the classes of the theme swatches on the page, if any.
+     */
+    updateActiveTheme = function() {
+        $('#themes .theme[data-theme-name="' + curTheme + '"]').
+            addClass('active').
+            siblings().
+            removeClass('active');
+    },
 
     updateImages = function() {
         $('img.logo.invertible').each(function() {
@@ -18,47 +91,42 @@ yuiCompress: !!bool true
         });
     },
 
-    switchTo = function(theme, isInverse) {
-        // save theme name globally
-        curTheme = theme;
-        curThemeIsInverse = isInverse;
-        // update stylesheet disabledness
+    /**
+     * Update disabled stylesheets.
+     */
+    updateStylesheets = function() {
         var links = $('link[rel$=stylesheet]');
         links.each(function() {
             this.disabled = true;
         });
-        $('link[rel$=stylesheet][title=theme-' + theme + ']').get(0).disabled = false;
-        // update image references
-        updateImages();
+        $('link[rel$=stylesheet][title=theme-' + curTheme + ']').get(0).disabled = false;
     };
 
     /**
      * Initialize handlers on page load.
      */
     $(function() {
-        // set current theme (todo: use cookies?)
-        curTheme = 'default';
-        // disable all but first stylesheet
-        $('link[rel$=stylesheet]:not(:eq(0))').each(function() {
-            this.disabled = true;
-        });
+        // set current theme, possibly use localStorage
+        if (!themeStorage('load')) {
+            curTheme = 'default';
+            curThemeIsInverse = false;
+        }
+        updateStylesheets();
+        updateImages();
+        updateActiveTheme();
         // enable switching
         $('body').on('click', '.theme', function() {
             var $link = $(this);
             if (!$link.hasClass('active')) {
                 switchTo($link.data('theme-name'), $link.hasClass('theme-inverse'));
-                $link.addClass('active').siblings().removeClass('active');
+                updateActiveTheme();
             }
         });
         // update pages when they are loaded
         $(window).on('denvelop-navigated', function(e, prevPage, curPage) {
             // set active theme on home page
             if (curPage.id === 'home') {
-                $('#themes .theme').each(function() {
-                    var $theme = $(this);
-                    $theme.toggleClass('active',
-                        $theme.data('theme-name') === curTheme);
-                });
+                updateActiveTheme();
             }
             // possibly update images that need inversion
             updateImages();
