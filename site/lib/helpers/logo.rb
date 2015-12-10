c8528d956d9304eedbdf5985d74dbbcfd1712dd0
Thom Castermans <thomcastermans@users.sourceforge.net>
2015-12-10 17:38:16 +0100

    Rename logos to work around AdBlock

--- site/lib/helpers/logo.rb
+++ site/lib/helpers/logo.rb
@@ -1,10 +1,12 @@
 module LogoHelper
 
-    def logo(name, href = nil, extraClasses = nil)
+    def logo(name, href = nil, extraClasses = nil, fileName = nil)
         (href.nil? ? '' : '<a href="' + href + '">') +
         '<img class="logo' +
         (extraClasses.nil? ? '' : ' ' + extraClasses) +
-        '" src="' + @items['/img/logos/' + name.downcase + '.*'].path + '" alt="' + name + ' logo" title="' + name + ' logo" />' +
+        '" src="' + @items['/img/logos/' +
+        (fileName.nil? ? name.downcase : fileName) + '.*'].path +
+        '" alt="' + name + ' logo" title="' + name + ' logo" />' +
         (href.nil? ? '' : '</a>')
     end
 
