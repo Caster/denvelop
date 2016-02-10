51e67d96aac6b690c71dc7f273f54db195cf3d56
Thom Castermans <thomcastermans@users.sourceforge.net>
2016-02-10 05:36:37 +0100

    Improve/fix blog display
    First of all, one of the pages with four images was not displaying
    correctly anymore because of my changes and using a helper function.
    
    Secondly, pages were not aligned correctly in Chrome, because
    apparently the font rendering is slightly different in Firefox and
    Chrome. Was to be expected of course. May have to test other browsers
    too actually. Oh well.

--- site/lib/helpers/blog.rb
+++ site/lib/helpers/blog.rb
@@ -18,12 +18,21 @@ module BlogHelper
             href = 'javascript:void(0);'
         end
         result = "<a class=\"#{classes}\" href=\"#{href}\">"
+        extra_attrs = (opts[:extra_attrs] ? opts[:extra_attrs] : '')
         if opts[:type] === 'pack-4'
             for i in 1..4
-                result += "<img src=\"#{sprintf isrc, i}\" />"
+                extra_attr = ''
+                if opts[:extra_attrs].kind_of?(Array) and
+                        opts[:extra_attrs].length >= i and
+                        not opts[:extra_attrs][i - 1].nil?
+                    for attr_key, attr_value in opts[:extra_attrs][i - 1]
+                        extra_attr += ' ' + attr_key.to_s + '="' + attr_value + '"'
+                    end
+                end
+                result += "<img src=\"#{sprintf isrc, i}\"#{extra_attr} />"
             end
         else
-            result += "<img src=\"#{isrc}\" />"
+            result += "<img src=\"#{isrc}\"#{extra_attrs} />"
         end
         return result + "</a>#{caption}"
     end
